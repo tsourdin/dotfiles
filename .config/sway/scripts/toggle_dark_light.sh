ac52d739ace51aaf5022a19708ede0d2f2cfac32
Thibault Sourdin <thibault.sourdin@mailo.com>
2024-05-17 19:09:51 +0200

    sway: update toggle script

--- .config/sway/scripts/toggle_dark_light.sh
+++ .config/sway/scripts/toggle_dark_light.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
 alacritty_config_file=/home/thibault/dotfiles/.config/alacritty/alacritty.toml
-alacritty_dark_theme=afterglow
-alacritty_light_theme=ayu_light
+alacritty_dark_theme=catppucin-frappe
+alacritty_light_theme=catppuccin-latte
+
+tmux_config_file=/home/thibault/dotfiles/.tmux.conf
+tmux_dark_theme=frappe
+tmux_light_theme=latte
 
 gtk_theme=$(gsettings get org.gnome.desktop.interface gtk-theme)
+
 if [ "$gtk_theme" = "'Yaru-dark'" ]
 then
     gsettings set org.gnome.desktop.interface gtk-theme 'Yaru'
     gsettings set org.gnome.desktop.interface color-scheme 'default'
     gsettings set org.gnome.desktop.interface icon-theme 'Yaru'
-    sed -i "s/\(.*$alacritty_dark_theme.*\)/# \1/g" "$alacritty_config_file" # comment afterglow theme
-    sed -i "s/^# \(.*$alacritty_light_theme.*\)/\1/g" "$alacritty_config_file" # uncomment ayu_light theme
+    sed -i "s/$alacritty_dark_theme/$alacritty_light_theme/" "$alacritty_config_file"
+    sed -i "s/$tmux_dark_theme/$tmux_light_theme/" "$tmux_config_file"
 elif [ "$gtk_theme" = "'Yaru'" ] # if light theme : we switch to dark
 then
     gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark'
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
     gsettings set org.gnome.desktop.interface icon-theme 'Yaru-dark'
-    sed -i "s/\(.*$alacritty_light_theme.*\)/# \1/g" "$alacritty_config_file" # comment ayu_light theme
-    sed -i "s/^# \(.*afterglow.*\)/\1/g" "$alacritty_config_file" # uncomment afterglow theme
+    sed -i "s/$alacritty_light_theme/$alacritty_dark_theme/" "$alacritty_config_file"
+    sed -i "s/$tmux_light_theme/$tmux_dark_theme/" "$tmux_config_file"
 fi
-
+tmux source-file "$tmux_config_file"
